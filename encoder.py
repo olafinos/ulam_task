0af1b4e940e2cd2c92595a7d815ca4bc1ed93f29
olafinos <olafinos@o2.pl>
2021-01-09 18:21:48 +0100

    Added encoder

---
+++ encoder.py
@@ -0,0 +1,80 @@
+from typing import List
+from itertools import permutations
+import random
+import re
+
+
+class WeirdTextEncoder:
+    """
+    Class for Weird Text encoding.
+
+    Permutates every word in a way that first and last character stays at their original place, when every other
+    characters is shuffled if possible. Also whitespaces, punctuations, etc. stays at their original places. Encoded
+    text is wrapped in a separator. It also adds sorted list of original words to the last line.
+    """
+
+    def __init__(self, input_text, separator="\n-weird-\n"):
+        self.separator = separator
+        self.regex_token = re.compile(r"(\w+)", re.U)
+        self.text = input_text
+        self.words = self._get_all_words_from_text(input_text)
+        self.shuffled_words = []
+
+    def _get_all_words_from_text(self) -> List[str]:
+        """
+        Separate all words from input text.
+        :return: List with input text words
+        """
+        return self.regex_token.findall(self.text)
+
+    def encode(self) -> str:
+        """
+        Encode input text to Weird Text format. Also adds separator between encoded text and sorted shuffled words.
+        :return: Encoded input text.
+        """
+        encoded_text = self._encode_text()
+        return self.create_final_output(encoded_text)
+
+    def _encode_text(self) -> str:
+        """
+        Create encoded text.
+        :return: Encoded input text.
+        """
+        encoded_text = self.text
+        for word in self.words:
+            encoded_word = self._encode_word(word)
+            encoded_text = re.sub(word, encoded_word, encoded_text)
+            if encoded_word != word:
+                self.shuffled_words.append(word)
+        return encoded_text
+
+    def _encode_word(self, word: str) -> str:
+        """
+        Encodes single word if possible. If its not it returns original word.
+        :param word: Word from input text.
+        :return: Encoded word if possible.
+        """
+        if len(word) <= 3 or len(set(word[1:-1])) == 1:
+            return word
+        else:
+            first_letter, last_letter, middle_part = word[0], word[-1], word[1:-1]
+            permutation_list = list(permutations(middle_part))
+            permutated_middle_part = random.choice(permutation_list)
+            permutation_list.remove(permutated_middle_part)
+            while permutation_list:
+                if permutated_middle_part == tuple(middle_part):
+                    permutated_middle_part = random.choice(permutation_list)
+                    permutation_list.remove(permutated_middle_part)
+                else:
+                    break
+
+            return f"{first_letter}{''.join(permutated_middle_part)}{last_letter}"
+
+    def create_final_output(self, encoded_text: str) -> str:
+        """
+        Adds separator and sorted shuffled words to encoded text
+        :param encoded_text: Encoded text.
+        :return: Separated encoded text with sorted shuffled words.
+        """
+        sorted_shuffled_words = " ".join(sorted(self.shuffled_words, key=str.lower))
+        return f"{self.separator}{encoded_text}{self.separator}{sorted_shuffled_words}"
