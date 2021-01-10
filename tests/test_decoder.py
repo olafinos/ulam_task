a97f998b580691527b3964046b919090e869888f
olafinos <olafinos@o2.pl>
2021-01-10 19:10:21 +0100

    Reformated code

--- tests/test_decoder.py
+++ tests/test_decoder.py
@@ -32,25 +32,40 @@ class TestWeirdTextDecoder(TestCase):
         self.assertEqual(self.decoder.shuffled_words, expected_words)
 
     def test_original_words_attribute(self):
-        expected_words = ['long', 'looong', 'sentence', 'some', 'test', 'This', 'with', 'words']
+        expected_words = [
+            "long",
+            "looong",
+            "sentence",
+            "some",
+            "test",
+            "This",
+            "with",
+            "words",
+        ]
         self.assertEqual(self.decoder.original_words, expected_words)
-        
+
     def test_check_if_middle_part_equal(self):
-        original_word = 'word'
-        permuted_word = 'wrod'
-        self.assertTrue(self.decoder._check_if_middle_part_chars_equal(original_word,permuted_word))
-        self.assertFalse(self.decoder._check_if_middle_part_chars_equal(original_word,"wood"))
+        original_word = "word"
+        permuted_word = "wrod"
+        self.assertTrue(
+            self.decoder._check_if_middle_part_chars_equal(original_word, permuted_word)
+        )
+        self.assertFalse(
+            self.decoder._check_if_middle_part_chars_equal(original_word, "wood")
+        )
 
     def test_decode_word(self):
-        word = 'loonog'
-        expected_word = 'looong'
+        word = "loonog"
+        expected_word = "looong"
         self.assertEqual(self.decoder._decode_word(word), expected_word)
-        self.assertEqual(self.decoder._decode_word('big'), 'big')
+        self.assertEqual(self.decoder._decode_word("big"), "big")
 
     def test_wrong_input(self):
         with self.assertRaises(ValueError):
             WeirdTextDecoder("WRONG_INPUT")
 
     def test_decode(self):
-        expected_text = 'This is a long looong test sentence,\nwith some big (biiiiig) words!'
-        self.assertEqual(self.decoder.decode(),expected_text)
+        expected_text = (
+            "This is a long looong test sentence,\nwith some big (biiiiig) words!"
+        )
+        self.assertEqual(self.decoder.decode(), expected_text)
