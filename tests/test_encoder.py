76bbd7c20929997eba6d18bcd87e51b642ccc695
olafinos <olafinos@o2.pl>
2021-01-10 16:31:31 +0100

    moved decoder and encoder to separate module

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -1,14 +1,24 @@
 from unittest import TestCase
-from ..encoder import WeirdTextEncoder
+from unittest.mock import patch
+from weird_text.encoder import WeirdTextEncoder
+from random import Random
 
-INPUT_TEXT = "‘This is a long looong test sentence,\n’‘with some big (biiiiig) words!’"
+
 class TestWeirdTextEncoder(TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.input_text = INPUT_TEXT
+        cls.input_text = "‘This is a long looong test sentence,\n’‘with some big (biiiiig) words!’"
+        cls.random = Random(42)
         cls.encoder = WeirdTextEncoder(cls.input_text)
 
     def test_get_all_words_from_text(self):
         expected = ['This', 'is', 'a', 'long', 'looong', 'test', 'sentence', 'with', 'some', 'big', 'biiiiig', 'words']
         self.assertEqual(self.encoder.words, expected)
+
+    @patch('weird_text.encoder.random')
+    def test_encode_word(self, random_choice_mock):
+        random_choice_mock.choice._mock_side_effect = self.random.choice
+        encoded_word = self.encoder._encode_word('word')
+        self.assertEqual(encoded_word, 'wrod')
+
